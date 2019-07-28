d836cc62576043cb5edccd77086c7df0049be5c7
Seth Lachman <lachmanseth17@gmail.com>
2019-07-28 10:50:34 -0400

    Implemented reverse function.

---
+++ performance-js/reverse.js
@@ -0,0 +1,10 @@
+const reverse = str => {
+  let reversed = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  return reversed;
+}
+
+reverse('')                         // ''
+reverse('abcdef')                   // 'fedcba'
