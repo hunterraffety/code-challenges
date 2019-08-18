616a547ce3244f6359deccfdc43fa3ff49d2276e
Seth Lachman <lachmanseth17@gmail.com>
2019-08-18 18:04:45 -0400

    Completed jewel-in-stone exercise.

---
+++ leetcode/jewels-stones.js
@@ -0,0 +1,26 @@
+var numJewelsInStones = function(J, S) {
+  let result = 0;
+  for (let i = 0; i < S.length; i++) {
+    if (J.includes(S[i])) {
+      result++;
+    }
+  }
+  return result;
+};
+
+// Alternate method
+var numJewelsInStones = function(J, S) {
+  let result = 0;
+  let arr = S.split("");
+  for (let i = 0; i < arr.length; i++) {
+    if (J.indexOf(arr[i]) > -1) {
+      result++;
+    }
+  }
+  return result;
+};
+
+numJewelsInStones("aA", "aAAbbbb");
+// 3
+numJewelsInStones("z", "ZZ");
+// 0
